0d4678fb3746de3c872891b1c6fcac1642700d23
SvetozarMateev <vidic9898@gmail.com>
2019-03-16 09:50:58 +0200

    Initial with db

---
+++ FavourAPI/Migrations/20190316060251_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FavourAPI.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    TestId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.TestId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
---
+++ FavourAPI/WorkFavourDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FavourAPI
+{
+    public class WorkFavourDbContext : DbContext
+    {
+        public WorkFavourDbContext(DbContextOptions<WorkFavourDbContext> options)
+            : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
