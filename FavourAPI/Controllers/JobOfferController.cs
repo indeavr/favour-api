592d8b786944bda89d87f725347b4b9e226be799
Donevski <donevskimartin@gmail.com>
2019-03-24 01:14:03 +0200

    slavery

--- FavourAPI/Controllers/JobOfferController.cs
+++ FavourAPI/Controllers/JobOfferController.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using FavourAPI.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FavourAPI.Controllers
 {
-    public class JobOfferController
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class JobOfferController : ControllerBase
     {
+        private readonly IJobOfferService jobOfferService;
+
+        public JobOfferController([FromServices] IJobOfferService service)
+        {
+            this.jobOfferService = service;
+        }
+
+        [HttpGet]
+        public ActionResult<JobOfferDto> Get()
+        {
+            var jobList = jobOfferService.
+            return Ok();
+        }
     }
 }
