40a8471c33b8a9441eb6c0a9c49292fc9acdb5fd
SvetozarMateev <vidic9898@gmail.com>
2019-03-24 03:44:04 +0200

    Refactored the job offer

---
+++ FavourAPI/Dtos/PeriodDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FavourAPI.Dtos
+{
+    public class PeriodDto
+    {
+        public long StartDate { get; set; }
+
+        public long EndDate { get; set; }
+
+        public long StartTime { get; set; }
+
+        public long EndTime { get; set; }
+    }
+}
