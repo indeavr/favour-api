c85490c379a6832275f134170b91828d0e79fdad
Donevski <donevskimartin@gmail.com>
2019-05-23 18:55:38 +0300

    added Resul Object for error handling and consistency

---
+++ FavourAPI.Services/Helpers/Result/InvalidResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavourAPI.Services.Helpers.Result
+{
+    public class InvalidResult<T> : Result<T>
+    {
+        private string errors;
+        public InvalidResult(string errors)
+        {
+            this.errors = errors;
+        }
+
+        public override ResultType Type => ResultType.Invalid;
+
+        public override List<string> Errors => new List<string> { this.errors ?? "The input was invalid." };
+
+        public override T Data => default(T);
+    }
+}
