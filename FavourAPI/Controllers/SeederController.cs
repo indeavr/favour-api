f229caf2ade59029d07d2635a0362077ddb6883e
Nikolay Lubomirov <origamista99@gmail.com>
2019-06-01 20:14:41 +0200

    Seeding controller

---
+++ FavourAPI/Controllers/SeederController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FavourAPI.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FavourAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SeederController : Controller
+    {
+        private readonly IDatabaseSeeder databaseSeeder;
+
+        public SeederController([FromServices] IDatabaseSeeder databaseSeeder)
+        {
+            this.databaseSeeder = databaseSeeder;
+        }
+
+        //[HttpPost("seed")]
+        public IActionResult Index()
+        {
+            databaseSeeder.SeedData();
+
+            return Ok("Dabase is seeded successfully!");
+        }
+    }
+}
