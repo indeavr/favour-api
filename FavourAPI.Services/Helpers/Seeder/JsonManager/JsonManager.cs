7b98e1a392c138324d87a31be83f334d9410c563
Nikolay Lubomirov <origamista99@gmail.com>
2019-06-01 19:33:33 +0200

    Json Manager

---
+++ FavourAPI.Services/Helpers/Seeder/JsonManager/JsonManager.cs
@@ -0,0 +1,33 @@
+﻿using FavourAPI.Services.Helpers.Seeder.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FavourAPI.Services.Helpers.Seeder.JsonManager
+{
+    public class JsonManager : IJsonManager
+    {
+        public void SerializeJson<T>(T entity, string path)
+        {
+            using (StreamWriter file = File.CreateText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, entity);
+            }
+        }
+
+        public T DeserializeJson<T>(string path)
+        {
+            T entity;
+            using (StreamReader file = File.OpenText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                entity = (T)serializer.Deserialize(file, typeof(T));
+            }
+
+            return entity;
+        }
+    }
+}
